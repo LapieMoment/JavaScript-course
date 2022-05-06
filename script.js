a02c2bc3ba3e8f6f592998065db56bcf4e52ea11
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-06 17:10:35 +0200

    Comment/console.log()/let/const

---
+++ script.js
@@ -0,0 +1,24 @@
+//alert('Poprawny zapis');
+
+
+//Obsługa komentarzy:
+
+//pojedyńyczy komentarz
+
+/*
+wileo
+linijkowy
+komentarz
+*/
+
+
+//Obsługa konsoli
+console.log('hello');
+
+
+//Zmienne                           90% przypadków
+const name = 'Lista';
+
+//let - można ją nadpisać           10% przypadków
+let age =  32;
+let color;
