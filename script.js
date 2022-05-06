8ed1cbd7b4d2b1e5352423951cc4a987030fdb6d
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-06 17:19:35 +0200

    First exercise - name/age/dish

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-//alert('Poprawny zapis');
+const name = "Łukasz";
+let age = 23;
+const ulubionaPotrawa = "Golonka";
 
-
-//Obsługa komentarzy:
-
-//pojedyńyczy komentarz
-
-/*
-wileo
-linijkowy
-komentarz
-*/
-
-
-//Obsługa konsoli
-console.log('hello');
-
-
-//Zmienne                           90% przypadków
-const name = 'Lista';
-
-//let - można ją nadpisać           10% przypadków
-let age =  32;
-let color;
+console.log(name);
+console.log(age);
+console.log(ulubionaPotrawa);
