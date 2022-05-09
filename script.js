1fd50957b9e2d22950bf31aa7e9f1a16216c0442
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-09 19:53:44 +0200

    Project #2 - sizeUp/Down/changeColor

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const img = document.querySelector('.item1')
-const btnArrow = document.querySelector('.arrow')
-const icon = document.querySelector('.fas')
+const colorBtn = document.querySelector('.color')
+const p = document.querySelector('p')
+const sizeUp = document.querySelector('.sizeUp')
+const sizeDown = document.querySelector('.sizeDown')
 
-const showImg = () => {
-	img.classList.toggle('hide')
-	if(img.classList.contains('hide')){
-		icon.style.transform = 'rotate(180deg)'
-	}
-	else{
-		icon.style.transform = 'rotate(0deg)'
+let fontSize = 36
+
+const increase = () => {
+	if(fontSize <=40){
+		fontSize = fontSize + 2;
+		p.style.fontSize = `${fontSize}px`
 	}
 }
+const insizedown = () => {
+	if(fontSize >= 20){
+		fontSize = fontSize - 2;
+		p.style.fontSize = `${fontSize}px`
+	}
+}
+const changeColor = () => {
+	let z = (Math.round(Math.random()*999999))
+	p.style.color = `#${z}`
+}
 
-btnArrow.addEventListener('click', showImg)
+
+colorBtn.addEventListener('click',changeColor)
+sizeUp.addEventListener('click',increase)
+sizeDown.addEventListener('click',insizedown)
+
