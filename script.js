7281072e5f9223b767e62ef9399bf66111f3630e
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-11 00:11:31 +0200

    Project #4 Converter C to F and F to C (deegres)

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-const pass = document.querySelector('#password');
-const p = document.querySelector('.passinfo');
-const letters = /[a-z]/i;
-const numbers = /[0-9]/;
-const special = /[!@#$%^&*()]/;
-const minValue = 10;
+const btnConv = document.querySelector('.conv')
+const btnReset = document.querySelector('.reset')
+const btnChange = document.querySelector('.change')
+const valueS = document.querySelector('#converter')
+const result = document.querySelector('.result')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
 
-const showMsg = () => {
-	if(pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)){
-		p.textContent = 'Masz bardzo dobre hasło!'
-		p.style.color = 'lime'
-	}else if(pass.value.length > minValue){
-		p.textContent = 'Masz dobre hasło!'
-		p.style.color = 'gold'
-	}
-	else{
-		p.textContent = 'Masz słabe hasło!'
-		p.style.color = 'tomato'
+const convert = () => {
+	if (valueS.value !== '' && one.textContent === '°C') {
+		let wynik = (valueS.value * 1.8 + 32).toFixed(2)
+		result.textContent = valueS.value + `°C to ${wynik} °F`
+		valueS.value = ''
+	} else if(valueS.value !== '' && one.textContent === '°F'){
+		let wynik = ((valueS.value - 32) / 1.8).toFixed(2)
+		result.textContent = valueS.value + `°F to ${wynik} °C`
+		valueS.value = ''
 	}
 }
-const checkPassword = () => {
-	if(pass.value == ''){
-		p.textContent = 'Nie podałeś hasła...'
-		p.style.color = ''	
-	}else{
-		showMsg()
+
+const reset = () => {
+	result.textContent = ''
+	valueS.value = ''
+}
+
+const change = () => {
+	if (one.textContent === '°C') {
+		one.textContent = '°F'
+		two.textContent = '°C'
+	} else {
+		one.textContent = '°C'
+		two.textContent = '°F'
 	}
 }
-pass.addEventListener('keyup',checkPassword)
+
+btnConv.addEventListener('click', convert)
+btnReset.addEventListener('click', reset)
+btnChange.addEventListener('click', change)
+
+//°F = (°C × 1.8) + 32
+//°C = (°F − 32) /1.8
