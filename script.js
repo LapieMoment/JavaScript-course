b402efab964fbf6a9c3d60e2bc5ae643e21ca6de
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-11 14:15:47 +0200

    Project #7 First

--- script.js
+++ script.js
@@ -1,26 +1,77 @@
-const currentDay = document.querySelector('.current-day')
-const funFact = document.querySelector('.fun-fact')
-const day = new Date();
+const input = document.querySelector('.todo-input')
+const errorInfo = document.querySelector('.error-info')
+const btnAddTask = document.querySelector('.btn-add')
+const popup = document.querySelector('.popup')
+const popupBtnCancel = document.querySelector('.cancel')
+const btnDeleteTask1 = document.querySelector('[data-id="test1"] .delete')
+const btnDeleteTask2 = document.querySelector('[data-id="test2"] .delete')
+const btnDeleteTask3 = document.querySelector('[data-id="test3"] .delete')
+const editBtnTask1 = document.querySelector('[data-id="test1"] .edit')
+const editBtnTask2 = document.querySelector('[data-id="test2"] .edit')
+const editBtnTask3 = document.querySelector('[data-id="test3"] .edit')
+const task1 = document.querySelector('[data-id="test1"]')
+const task2 = document.querySelector('[data-id="test2"]')
+const task3 = document.querySelector('[data-id="test3"]')
 
-const facts = [
-	'Krokodyl nie potrafi wystawić języka.',
-	'Każdy człowiek spędził około pół godziny jako pojedyncza komórka.',
-	'Dźwięk przemieszcza się 15 razy szybciej przez stal niż przez powietrze.',
-	'Leniwce potrzebują dwóch tygodni na strawienie jedzenia.',
-	'Goryle śpią nawet czternaście godzin dziennie.',
-	'Język kameleona jest dwukrotnie dłuższy od jego ciała.',
-	'Chińczycy w ciągu roku zużywają około 80 miliardów pałeczek.',
-	'Żeby wejść na Wieżę Eiffla trzeba pokonać aż 1710 stopni.'
-]
-
-const dayOfWeekName = new Date().toLocaleString('default', {weekday: 'long'});
-
-const show = () => {
-	let rand = Math.round(Math.random()*facts.length-1);
-	console.log(rand);
-	currentDay.textContent = dayOfWeekName
-	funFact.textContent = facts[rand]
+//console.log(input.textContent = 'siema');
+const checkTask = () => {
+	if (input.value.length > 0 && task1.style.display !== 'flex') {
+		taskValue(task1)
+		console.log(task1.firstChild.textContent);
+		console.log(task2.firstChild.textContent);
+	} else if (task2.style.display !== 'flex' && task1.firstChild.textContent !== input.value) {
+		taskValue(task2)
+		
+	} else if (task3.style.display !== 'flex' && task3.firstChild.textContent !== task2.firstChild.textContent  && task3.firstChild.textContent !== task1.firstChild.textContent){
+		taskValue(task3)
+	} else {
+		errorInfo.textContent = 'Nie możesz dodać tego samego zadania'
+		errorInfo.style.color = 'tomato'
+	}
 }
 
-window.addEventListener('load',show)
+const taskValue = (task) => {
+			task.style.display = 'flex'
+			task.firstChild.textContent = input.value
+			errorInfo.textContent = ''
+			errorInfo.style.color = ''
+}
 
+const deleteTask1 = () => {
+	task1.style.display = 'none'
+}
+const deleteTask2 = () => {
+	task2.style.display = 'none'
+}
+const deleteTask3 = () => {
+	task3.style.display = 'none'
+}
+
+const editTask1 = () => {
+	popup.style.display = 'flex'
+}
+const editTask2 = () => {
+	popup.style.display = 'flex'
+}
+const editTask3 = () => {
+	popup.style.display = 'flex'
+}
+const popupBtnCancelFunction = () => {
+	popup.style.display = 'none'
+}
+
+popupBtnCancel.addEventListener('click', popupBtnCancelFunction)
+
+editBtnTask1.addEventListener('click', editTask1)
+editBtnTask2.addEventListener('click', editTask2)
+editBtnTask3.addEventListener('click', editTask3)
+
+editBtnTask1.addEventListener('click', editTask1)
+editBtnTask2.addEventListener('click', editTask2)
+editBtnTask3.addEventListener('click', editTask3)
+
+btnDeleteTask1.addEventListener('click', deleteTask1)
+btnDeleteTask2.addEventListener('click', deleteTask2)
+btnDeleteTask3.addEventListener('click', deleteTask3)
+
+btnAddTask.addEventListener('click', checkTask)
