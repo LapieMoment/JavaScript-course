b92ccc1b230e1279d6c92b022ecad882f969b140
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-11 15:13:21 +0200

    Project #7 Second approach

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const input = document.querySelector('.todo-input')
+const inputEdit = document.querySelector('.popup-input')
 const errorInfo = document.querySelector('.error-info')
 const btnAddTask = document.querySelector('.btn-add')
 const popup = document.querySelector('.popup')
@@ -9,69 +10,73 @@ const btnDeleteTask3 = document.querySelector('[data-id="test3"] .delete')
 const editBtnTask1 = document.querySelector('[data-id="test1"] .edit')
 const editBtnTask2 = document.querySelector('[data-id="test2"] .edit')
 const editBtnTask3 = document.querySelector('[data-id="test3"] .edit')
+const c1 = document.querySelector('[data-id="test1"] .complete')
+const c2 = document.querySelector('[data-id="test2"] .complete')
+const c3 = document.querySelector('[data-id="test3"] .complete')
 const task1 = document.querySelector('[data-id="test1"]')
 const task2 = document.querySelector('[data-id="test2"]')
 const task3 = document.querySelector('[data-id="test3"]')
 
-//console.log(input.textContent = 'siema');
 const checkTask = () => {
 	if (input.value.length > 0 && task1.style.display !== 'flex') {
 		taskValue(task1)
-		console.log(task1.firstChild.textContent);
-		console.log(task2.firstChild.textContent);
 	} else if (task2.style.display !== 'flex' && task1.firstChild.textContent !== input.value) {
 		taskValue(task2)
-		
-	} else if (task3.style.display !== 'flex' && task3.firstChild.textContent !== task2.firstChild.textContent  && task3.firstChild.textContent !== task1.firstChild.textContent){
+	} else if (
+		task3.style.display !== 'flex' &&
+		task1.firstChild.textContent !== input.value &&
+		task2.firstChild.textContent !== input.value
+	) {
 		taskValue(task3)
+	} else if (task3.style.display == 'flex') {
+		errorInfo.textContent = 'Nie możesz dodać czwartego zadania'
+		errorInfo.style.color = 'gold'
 	} else {
 		errorInfo.textContent = 'Nie możesz dodać tego samego zadania'
 		errorInfo.style.color = 'tomato'
 	}
 }
 
-const taskValue = (task) => {
-			task.style.display = 'flex'
-			task.firstChild.textContent = input.value
-			errorInfo.textContent = ''
-			errorInfo.style.color = ''
+const taskValue = task => {
+	task.style.display = 'flex'
+	task.firstChild.textContent = input.value
+	errorInfo.textContent = ''
+	errorInfo.style.color = ''
 }
 
 const deleteTask1 = () => {
 	task1.style.display = 'none'
+	errorInfo.textContent = ''
 }
 const deleteTask2 = () => {
 	task2.style.display = 'none'
+	errorInfo.textContent = ''
 }
 const deleteTask3 = () => {
 	task3.style.display = 'none'
+	errorInfo.textContent = ''
+}
+const editTask = () => {
+	popup.style.display = 'flex'
 }
 
-const editTask1 = () => {
-	popup.style.display = 'flex'
-}
-const editTask2 = () => {
-	popup.style.display = 'flex'
-}
-const editTask3 = () => {
-	popup.style.display = 'flex'
-}
 const popupBtnCancelFunction = () => {
 	popup.style.display = 'none'
 }
 
-popupBtnCancel.addEventListener('click', popupBtnCancelFunction)
+const completeTask1 = () => {
+	console.log('siema')
+}
 
-editBtnTask1.addEventListener('click', editTask1)
-editBtnTask2.addEventListener('click', editTask2)
-editBtnTask3.addEventListener('click', editTask3)
+c1.addEventListener('click', completeTask1)
 
-editBtnTask1.addEventListener('click', editTask1)
-editBtnTask2.addEventListener('click', editTask2)
-editBtnTask3.addEventListener('click', editTask3)
+editBtnTask1.addEventListener('click', editTask)
+editBtnTask2.addEventListener('click', editTask)
+editBtnTask3.addEventListener('click', editTask)
 
 btnDeleteTask1.addEventListener('click', deleteTask1)
 btnDeleteTask2.addEventListener('click', deleteTask2)
 btnDeleteTask3.addEventListener('click', deleteTask3)
 
 btnAddTask.addEventListener('click', checkTask)
+popupBtnCancel.addEventListener('click', popupBtnCancelFunction)
