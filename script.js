c01823b029d915e99a79887eae9f370dd5344d6f
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-12 00:41:46 +0200

    Project #7 Teacher code example

--- script.js
+++ script.js
@@ -1,82 +1,108 @@
-const input = document.querySelector('.todo-input')
-const inputEdit = document.querySelector('.popup-input')
-const errorInfo = document.querySelector('.error-info')
-const btnAddTask = document.querySelector('.btn-add')
-const popup = document.querySelector('.popup')
-const popupBtnCancel = document.querySelector('.cancel')
-const btnDeleteTask1 = document.querySelector('[data-id="test1"] .delete')
-const btnDeleteTask2 = document.querySelector('[data-id="test2"] .delete')
-const btnDeleteTask3 = document.querySelector('[data-id="test3"] .delete')
-const editBtnTask1 = document.querySelector('[data-id="test1"] .edit')
-const editBtnTask2 = document.querySelector('[data-id="test2"] .edit')
-const editBtnTask3 = document.querySelector('[data-id="test3"] .edit')
-const c1 = document.querySelector('[data-id="test1"] .complete')
-const c2 = document.querySelector('[data-id="test2"] .complete')
-const c3 = document.querySelector('[data-id="test3"] .complete')
-const task1 = document.querySelector('[data-id="test1"]')
-const task2 = document.querySelector('[data-id="test2"]')
-const task3 = document.querySelector('[data-id="test3"]')
+let todoInput, errorInfo, addBtn, ulList, newTodo, popup, popupInfo, todoToEdit, popupInput, popupAddBtn, popupCloseBtn
 
-const checkTask = () => {
-	if (input.value.length > 0 && task1.style.display !== 'flex') {
-		taskValue(task1)
-	} else if (task2.style.display !== 'flex' && task1.firstChild.textContent !== input.value) {
-		taskValue(task2)
-	} else if (
-		task3.style.display !== 'flex' &&
-		task1.firstChild.textContent !== input.value &&
-		task2.firstChild.textContent !== input.value
-	) {
-		taskValue(task3)
-	} else if (task3.style.display == 'flex') {
-		errorInfo.textContent = 'Nie możesz dodać czwartego zadania'
-		errorInfo.style.color = 'gold'
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist ul')
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
+}
+
+const prepareDOMEvents = () => {
+	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkClick)
+	popupCloseBtn.addEventListener('click', closePopu)
+	popupAddBtn.addEventListener('click', acceptPopu)
+	todoInput.addEventListener('keyup', eneterPopu)
+}
+
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = todoInput.value
+		createToolsArea()
+		ulList.append(newTodo)
+		todoInput.value = ''
+		errorInfo.textContent = ''
 	} else {
-		errorInfo.textContent = 'Nie możesz dodać tego samego zadania'
-		errorInfo.style.color = 'tomato'
+		errorInfo.textContent = 'Wpisz treść zadania!'
 	}
 }
 
-const taskValue = task => {
-	task.style.display = 'flex'
-	task.firstChild.textContent = input.value
-	errorInfo.textContent = ''
-	errorInfo.style.color = ''
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div')
+	toolsPanel.classList.add('tools')
+
+	const completeBtn = document.createElement('button')
+	completeBtn.classList.add('complete')
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+	const editBtn = document.createElement('button')
+	editBtn.classList.add('edit')
+	editBtn.textContent = 'EDIT'
+
+	const deleteBtn = document.createElement('button')
+	deleteBtn.classList.add('delete')
+	deleteBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+	newTodo.append(toolsPanel)
 }
 
-const deleteTask1 = () => {
-	task1.style.display = 'none'
-	errorInfo.textContent = ''
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+		e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTodo(e)
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e)
+	}
 }
-const deleteTask2 = () => {
-	task2.style.display = 'none'
-	errorInfo.textContent = ''
-}
-const deleteTask3 = () => {
-	task3.style.display = 'none'
-	errorInfo.textContent = ''
-}
-const editTask = () => {
+
+const editTodo = e => {
 	popup.style.display = 'flex'
+	todoInput = e.target.closest('li')
+	popupInput.value = todoInput.firstChild.textContent
 }
 
-const popupBtnCancelFunction = () => {
+const acceptPopu = () => {
+	if (popupInput.value !== '') {
+		todoInput.firstChild.textContent = popupInput.value
+		popup.style.display = 'none'
+		popupInfo.textContent = ''
+	} else {
+		popupInfo.textContent = 'Podaj treść'
+	}
+}
+
+const closePopu = () => {
 	popup.style.display = 'none'
 }
 
-const completeTask1 = () => {
-	console.log('siema')
+const deleteTodo = e => {
+	e.target.closest('li').remove()
+	const allTodos = ulList.querySelectorAll('li')
+	if (allTodos.length == 0) {
+		errorInfo.textContent = 'Dodaj zadania'
+	} else {
+		errorInfo.textContent = ''
+	}
 }
 
-c1.addEventListener('click', completeTask1)
+const eneterPopu = (e) => {
+	if(e.key==='Enter' ){
+		addNewTodo()
+	}
+}
 
-editBtnTask1.addEventListener('click', editTask)
-editBtnTask2.addEventListener('click', editTask)
-editBtnTask3.addEventListener('click', editTask)
-
-btnDeleteTask1.addEventListener('click', deleteTask1)
-btnDeleteTask2.addEventListener('click', deleteTask2)
-btnDeleteTask3.addEventListener('click', deleteTask3)
-
-btnAddTask.addEventListener('click', checkTask)
-popupBtnCancel.addEventListener('click', popupBtnCancelFunction)
+document.addEventListener('DOMContentLoaded', main)
