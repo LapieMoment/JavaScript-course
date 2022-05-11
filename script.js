0b2f9a9b4ee30765c241a8b448e047f94761f119
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-11 11:30:49 +0200

    Project #6 Simple show current day and facts

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const btn = document.querySelector('.burger')
-const uList = document.querySelector('nav ul')
-const iconBurger = document.querySelector('.fa-bars')
-const iconExit = document.querySelector('.fa-times')
+const currentDay = document.querySelector('.current-day')
+const funFact = document.querySelector('.fun-fact')
+const day = new Date();
 
-const change = () => {
-	uList.classList.toggle('active')
-	btn.classList.toggle('active')
-	iconExit.classList.toggle('hide')
-	iconBurger.classList.toggle('hide')
+const facts = [
+	'Krokodyl nie potrafi wystawić języka.',
+	'Każdy człowiek spędził około pół godziny jako pojedyncza komórka.',
+	'Dźwięk przemieszcza się 15 razy szybciej przez stal niż przez powietrze.',
+	'Leniwce potrzebują dwóch tygodni na strawienie jedzenia.',
+	'Goryle śpią nawet czternaście godzin dziennie.',
+	'Język kameleona jest dwukrotnie dłuższy od jego ciała.',
+	'Chińczycy w ciągu roku zużywają około 80 miliardów pałeczek.',
+	'Żeby wejść na Wieżę Eiffla trzeba pokonać aż 1710 stopni.'
+]
+
+const dayOfWeekName = new Date().toLocaleString('default', {weekday: 'long'});
+
+const show = () => {
+	let rand = Math.round(Math.random()*facts.length-1);
+	console.log(rand);
+	currentDay.textContent = dayOfWeekName
+	funFact.textContent = facts[rand]
 }
 
-btn.addEventListener('click',change)
+window.addEventListener('load',show)
+
