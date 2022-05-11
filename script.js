01bab161a3c905afea596404745492db34f7247a
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-11 10:39:09 +0200

    Project #5 Burger Menu, better code

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
 const btn = document.querySelector('.burger')
-const uList = document.querySelector('ul')
-const iconBurger = document.querySelector('i')
-const iconExit = document.querySelector('i:nth-child(2)')
-
-	iconExit.classList.remove('fas')
-	iconExit.classList.remove('fa-bars')
-	
-const show = () => {
-	uList.classList.add("active")
-	btn.classList.add("active")
-	iconBurger.classList.remove('fas')
-	iconBurger.classList.remove('fa-bars')
-	iconExit.classList.add('fas')
-	iconExit.classList.add('fa-times')
-}
-
-const hidden = () => {
-	uList.classList.remove('active')	
-	btn.classList.remove('active')	
-	iconExit.classList.remove('fas')
-	iconExit.classList.remove('fa-times')
-	iconBurger.classList.add('fas')
-	iconBurger.classList.add('fa-bars')
-}
+const uList = document.querySelector('nav ul')
+const iconBurger = document.querySelector('.fa-bars')
+const iconExit = document.querySelector('.fa-times')
 
 const change = () => {
-	if(uList.classList.contains('active')){
-		hidden()
-	}else{
-		show()
-	}
+	uList.classList.toggle('active')
+	btn.classList.toggle('active')
+	iconExit.classList.toggle('hide')
+	iconBurger.classList.toggle('hide')
 }
 
 btn.addEventListener('click',change)
