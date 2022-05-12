c7f7d4db2e92a348568a17ce353b2eb8cdec9b53
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-12 22:19:41 +0200

    Project #8 WeatherApp First approach

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-const btn = document.querySelector('button')
-const img = document.querySelector('img')
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const warning = document.querySelector('.warning')
+const photo = document.querySelector('.photo')
+const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temperature')
+const humidity = document.querySelector('.humidity')
+const inputCity = document.querySelector('input')
+let currentCity = 'kraków'
+let currentTemp
+let currentWeather
+let currentHum
 
-const URL = 'https://dog.ceo/api/breeds/image/random'
+const APIKEY = '1d11bb9bfa373e7415dd8838e057b403'
 
-// fetch(URL).then().catch()
+const main = () => {
+	DOMElements()
+	DOMEvents()
+}
 
-//fetch - służy do pobrania danych
-//then - mówi co zrobimy z nimi kiedy wszystko pobierzemy
-//catch - wyłapuje błędy
-btn.addEventListener('click', () => {
+const DOMElements = () => {}
+
+const DOMEvents = () => {
+	button.addEventListener('click', response)
+}
+const response = () => {
+	currentCity = inputCity.value
+	console.log(currentCity)
+	let URL = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${APIKEY}&units=metric&lang=pl`
 	fetch(URL)
-		.then(response => response.json())
-		.then(data => img.setAttribute('src', data.message))
-})
+		.then(res => res.json())
+		.then(data => {
+			temperature.textContent = Math.round(data.main.temp) + "°C"
+			humidity.textContent = Math.round(data.main.humidity) + "%"
+			currentWeather = data.weather
+			console.log(currentWeather)
+			currentInformation = data
+		})
+		.catch(err => console.error(err))
+	cityName.textContent = inputCity.value.charAt(0).toUpperCase() + inputCity.value.slice(1)
+	inputCity.value = ''
+
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', main)
