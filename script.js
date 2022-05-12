d60b14ad2067bdccaf328169a00e51894a7e25dd
Łukasz Głos <lukaszglos@poczta.fm>
2022-05-12 16:01:57 +0200

    Section 12 Class

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-//konstruktor
-function User(name,age) {
+function Person(name, age) {
 	this.name = name
 	this.age = age
-	this.hello = function(){
-		console.log(`Cześć ${this.name}`);
+}
+
+Person.prototype.showName = function () {
+	console.log(`Mam na imie ${this.name}`)
+}
+Person.prototype.showAge = function () {
+	console.log(`Mam ${this.age} lat`)
+}
+
+const person1 = new Person('Maciek', 11)
+person1.showAge()
+person1.showName()
+
+//--------------------------------------------------------------------
+
+class Person2 {
+	constructor(name, age) {
+		this.name = name
+		this.age = age
+	}
+	showName() {
+		console.log(`Mam na imie ${this.name}`)
+	}
+	showAge() {
+		console.log(`Mam ${this.age} lat`)
 	}
 }
 
-//klasyczny obiekt
-const user1 = {
-	age: 21,
-	name: 'Klaudia'
-}
-console.log(user1);
-
-//Obiekt na bazie konstrutkora
-const user2 = new User('Klaudia',21)
-const user3 = new User('Malina',21)
-console.log(user2);
-
-user3.hello()
+const person2 = new Person2('Monia',22)
+person2.showAge()
+person2.showName()
